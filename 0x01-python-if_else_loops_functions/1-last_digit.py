3821da9db8d54312a7c9db4aa832cfd3dea8016d
abdou50 <abdelkaderraddadi988@gmail.com>
2022-04-26 15:52:27 +0300

    l

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ n = k % 10
 if number < 0:
     n = -n
 if n < 6 and n != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, n))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, n))
 elif n == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, n))
 else:
