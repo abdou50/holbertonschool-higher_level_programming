b1b7caf4698a500fc22a6f6908d6898ea97073db
abdou50 <abdelkaderraddadi988@gmail.com>
2022-04-29 01:14:24 +0300

    l

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys as m
+
+    a = 0
+    for i in range(len(m.argv) - 1):
+        a += int(m.argv[i + 1])
+    print("{}".format(a))
+git
