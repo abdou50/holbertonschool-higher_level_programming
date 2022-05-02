828263a78479cbd1028e9b6a94fc4d8ab0745171
abdou50 <abdelkaderraddadi988@gmail.com>
2022-05-02 13:54:16 +0300

    L

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new = []
+    for i in my_list:
+        if i % 2 == 0:
+            new.append(1 == 1)
+        else:
+            new.append(1 == 2)
+    return new
