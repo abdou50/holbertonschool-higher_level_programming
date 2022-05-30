9d01ea14e50ba75ef5838f7dc2a0babea629082b
abdou50 <abdelkaderraddadi988@gmail.com>
2022-05-31 00:38:21 +0300

    l

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class """
+    def __init__(self, size):
+        """class"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size * self.__size
