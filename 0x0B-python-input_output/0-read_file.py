ecfb2f0eaeba88d6d56d8fe7e33392d7df583fa6
abdou50 <abdelkaderraddadi988@gmail.com>
2022-05-31 21:10:52 +0300

    l

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """readfile"""
-    with open("filename","r", encoding="UTF-8") as no:
-        print (no.read())
+    with open(filename, mode="r", encoding="UTF-8") as no:
+        print (no.read(), end="")
