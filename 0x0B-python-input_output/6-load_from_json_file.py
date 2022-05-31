ae75d41ae68672dfca07029b4a7388d1a00cdb0e
abdou50 <abdelkaderraddadi988@gmail.com>
2022-05-31 21:44:40 +0300

    L

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" hihihih """
+import json
+
+
+def from_json_string(my_str):
+    """class"""
+    with open(filename, mode="r", encoding="Utf-8") as fk:
+        return json.load(fk)
