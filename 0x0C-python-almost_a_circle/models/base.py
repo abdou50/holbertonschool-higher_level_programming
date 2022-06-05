6babe7e5b628b6740c3831a0766a5e1ae7f4ab5a
abdou50 <abdelkaderraddadi988@gmail.com>
2022-06-05 13:56:03 +0300

    l

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,11 +3,11 @@
 
 
 class Base:
-	__nb_objects = 0
-	"""def function init """
-	def __init__(self, id=None):
-		if id is not None:
-			self.id = id
-		else:
-			Base.__nb_objects += 1
-			self.id = self.__nb_objects
+    __nb_objects = 0
+    """def function init """
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
