b180a56ef250e6348ea55bab5d8c78275ca945ff
abdou50 <abdelkaderraddadi988@gmail.com>
2022-06-06 17:45:34 +0300

    ;

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,10 +8,10 @@ from models.base import Base
 class Rectangle(Base):
     """def functions"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super() .__init__(id)
 
     @property
@@ -33,3 +33,65 @@ class Rectangle(Base):
     def y(self):
         """methode"""
         return self.__y
+
+    @width.setter
+    def width(self, value):
+        """methode"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """methode"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("heightmust be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """methode"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value <= 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """methode"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value <= 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """methode"""
+        return self.width * self.height
+
+    def __str__(self):
+        """methode"""
+        return "[Rectangle] ({}) {}/{} - {}/{}"
+        .format(self.id, self.x, self.y, self.width, self.height)
+
+    def display(self):
+        """methode"""
+        for i in range(self.hight):
+            for j in range(self.width):
+                print("#", end="")
+            print("")
+
+    def to_dictionary(self):
+        """methode"""
+        dic = {}
+        dic["id"] = self.id
+        dic["width"] = self.width
+        dic["height"] = self.height
+        dic["x"] = self.x
+        dic["y"] = self.y
+        return dic
