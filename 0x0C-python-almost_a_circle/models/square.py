2c68db00ccf92384ae5dfcb10974041996cdd2f4
abdou50 <abdelkaderraddadi988@gmail.com>
2022-07-29 18:21:00 +0300

    l

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" the square """
+
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """init methode"""
+
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """methode to write"""
+
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.__width)
+
+    @property
+    def size(self):
+        """getter methode."""
+
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """setter methode"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
+
+    def to_dictionary(self):
+        """set it to dictionary"""
+        dic = {}
+        dic["id"] = self.id
+        dic["size"] = self.size
+        dic["x"] = self.x
+        dic["y"] = self.y
+        return dic
