1810b4d696ec28083ce9b46d0714e1dd331d4e2a
abdou50 <abdelkaderraddadi988@gmail.com>
2022-08-01 14:21:58 +0300

    l

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('process');
+if (process.argv[2] && process.argv[3]) {
+  console.log(`${process.argv[2]} is ${process.argv[3]}`);
+} else if (process.argv[2]) {
+  console.log(process.argv[2] + ' is ' + 'undefined');
+} else {
+  console.log('undefined is undefined');
+}
