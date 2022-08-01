b6b352257c2a6b11dd3d41e2e6b62f301a62ef8f
abdou50 <abdelkaderraddadi988@gmail.com>
2022-08-01 15:14:44 +0300

    l

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('process');
+if (process.argv[2]) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
