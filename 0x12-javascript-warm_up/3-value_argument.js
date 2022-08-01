5eb4a108c37af4fa21f3594c1318c669847a0607
abdou50 <abdelkaderraddadi988@gmail.com>
2022-08-01 14:01:50 +0300

    l

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const process = require('process');
-if (process.argv[2] !== null) {
+if (process.argv[2]) {
   console.log(process.argv[2]);
 } else {
   console.log('No argument');
