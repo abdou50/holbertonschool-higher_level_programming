e92a3c052099338b7d944e9f5e633b2c80621b0a
abdou50 <abdelkaderraddadi988@gmail.com>
2022-08-01 14:41:35 +0300

    l

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const process = require('process');
-if (isNaN(process.argv[2]) === true) {
+if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
   console.log('My mumber : ' + parseInt(process.argv[2]));
