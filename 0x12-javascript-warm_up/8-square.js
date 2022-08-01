837624889edcf4ed3772ea747d8ed37e45f03b25
abdou50 <abdelkaderraddadi988@gmail.com>
2022-08-01 19:35:41 +0300

    l

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const process = require('process');
-let str ='';
+let str = '';
 if (process.argv[2]) {
-	for (let i = 0; i < process.argv[2]; i++){
-		str += 'X';
-	}
-	for (let i = 0; i < process.argv[2]; i++){
-		console.log(str);
-	}
+  for (let i = 0; i < process.argv[2]; i++) {
+    str += 'X';
+  }
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log(str);
+  }
 } else if (isNaN(process.argv[2])) {
-	console.log('Missing size');
+  console.log('Missing size');
 }
