5f8fdfa89b57a82b59f42664d0a98818254dd9d0
abdou50 <abdelkaderraddadi988@gmail.com>
2022-08-02 20:11:32 +0300

    l

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let string = '';
+    for (let i = 0; i < this.width; i++) {
+      string += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(string);
+    }
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.width * 2;
+    return (this.width, this.height);
+  }
+
+  rotate () {
+    let i = 0;
+    i = this.height;
+    this.height = this.width;
+    this.width = i;
+    return (this.width, this.height);
+  }
+}
+module.exports = Rectangle;
