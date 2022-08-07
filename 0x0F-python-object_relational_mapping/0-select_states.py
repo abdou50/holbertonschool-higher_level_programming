e48febcaa6edb1708a90bce899218bd8e2ae6f3b
abdou50 <abdelkaderraddadi988@gmail.com>
2022-08-07 04:34:37 +0300

    k

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """sql"""
-
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
     connected = MySQLdb.connect(
         host="localhost",
         port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
     )
     string = connected.cursor()
-    string.execute("SELECT * FROM states ORDRE BY states.id")
+    string.execute("SELECT *  states ORDRE BY states.id")
     rowfetch = cursor.fetchall()
     for row in rowfetch:
-        print(row)
+       print(row)
     string.close()
     connected.close()
