625eb84f90eaecd6c7bff060249728b6c328ae32
abdou50 <gadourraddadi@gmail.com>
2022-08-20 01:37:20 +0300

    l

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""urllib"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.post(argv[1], data={'email': argv[2]})
+    print(response.text)
+
