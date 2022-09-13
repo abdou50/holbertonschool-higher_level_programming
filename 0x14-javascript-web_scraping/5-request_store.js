85a5b07ac721a81b28aca28ed92f94d3eb4c3410
abdou50 <gadourraddadi@gmail.com>
2022-09-14 01:29:50 +0300

    l

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const axios = require('axios');
+const process = require('process');
+const fs = require('fs');
+const arg = process.argv[2];
+const arg1 = process.argv[3];
+axios.get(arg)
+	.then(function (response) {
+		fs.writeFile(arg1, response.data, function (err) {
+			if (err) throw err
+		});
+	});
