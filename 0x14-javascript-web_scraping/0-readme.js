8746a4dbe9d00adf194d493f6a04ac64c1c2903f
abdou50 <gadourraddadi@gmail.com>
2022-09-13 19:48:29 +0300

    l

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-let arg = process.argv[2];
+const arg = process.argv[2];
 fs.readFile(arg, function (err, data) {
-	if (err) {
-		console.log(err);
-	}
-	console.log(data);
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString());
 });
