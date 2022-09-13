c660c8d61f2829c1a197eed083e63f3c312d46ae
abdou50 <gadourraddadi@gmail.com>
2022-09-14 01:17:28 +0300

    l

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 const process = require('process');
 const arg = process.argv[2] - 1;
 axios.get('https://swapi-api.hbtn.io/api/films/')
-	.then(function (response) {
-		console.log(response.data.results[arg].title);
-	})
-	.catch(function (err) {
-		console.log(err);
-	});
+  .then(function (response) {
+    console.log(response.data.results[arg].title);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
