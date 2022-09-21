c0d09e5cdffdd17265972f6102f797887772b3bf
abdou50 <gadourraddadi@gmail.com>
2022-09-21 21:08:52 +0300

    l

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+$('DIV#red_header').click(function () {
+	if ($('header').hasClass('red')) {
+		$('header').removeClass('red')
+		$('header').addClass('red')
+	} else if ($('header').hasClass('green')) {
+		$('header').removeClass('green')
+		$('header').addClass('red')
+	}
+});
